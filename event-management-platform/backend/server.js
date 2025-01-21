fce50fc676117b39dd7bc4ca1ccb092596c7dc89
Usman Khan <usman918khan@gmail.com>
2025-01-21 20:58:48 +0500

    backend-revised-&-cleared

--- event-management-platform/backend/server.js
+++ event-management-platform/backend/server.js
@@ -1,15 +1,14 @@
 require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
-const dotenv = require('dotenv');
+
 const connectDB = require('./config/db');
 const authRoutes = require('./routes/authRoutes');
 const eventRoutes = require('./routes/eventRoutes');
 
-dotenv.config();
-connectDB();
-
 const app = express();
+
+connectDB();
 
 app.use(cors());
 app.use(express.json());
